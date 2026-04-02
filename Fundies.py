8d060f5d84adb8ceddaadedbd563bd6ad5011a09
ychu-leeward <yufu.leeward@gmail.com>
2026-04-02 14:33:35 -0500

    Update Fundies.py

--- Fundies.py
+++ Fundies.py
@@ -123,9 +123,12 @@ def get_content_data(content_id):
     return None
 
 def ercot_token():
-    uid = st.secrets["ercot"]["username"]
-    pwd = st.secrets["ercot"]["password"]
-    SUBSCRIPTION = st.secrets["ercot"]["subscription"]
+    # uid = st.secrets["ercot"]["username"]
+    # pwd = st.secrets["ercot"]["password"]
+    # SUBSCRIPTION = st.secrets["ercot"]["subscription"]
+    uid = "yufu.chu@leewardenergy.com"
+    pwd = "05d4gk4V!"
+    SUBSCRIPTION = "64f370c78f9a47cda040577c8bf7b13c"
     AUTH_URL = (
         f"https://ercotb2c.b2clogin.com/ercotb2c.onmicrosoft.com/B2C_1_PUBAPI-ROPC-FLOW/oauth2/v2.0/"
         f"token?username={uid}&password={pwd}"
