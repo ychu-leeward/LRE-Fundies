e00a77583cdcc324399c700bbbefe60276af4aba
ychu-leeward <yufu.leeward@gmail.com>
2026-04-21 10:24:23 -0500

    Update Fundies.py

--- Fundies.py
+++ Fundies.py
@@ -792,22 +792,22 @@ def get_color_for_value(value, min_val, max_val, reverse=False):
 
 def check_password():
 
-    if "password_correct" not in st.session_state:
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+    
+    if not st.session_state.authenticated:
         st.title("Goat Farmers Only")
         with st.form("login_form"):
-            st.text_input(
-                "Password",
-                type="password",
-                key="password"
-            )
-    
-            if st.session_state.get("password") == st.secrets.get("app", {}).get("password", ""):
-                st.session_state["password_correct"] = True
-                st.rerun()
-
-        return False
-
-    return True
+            password = st.text_input("Password:", type="password", key="password_input")
+            submitted = st.form_submit_button("Login", type="primary")
+            if submitted:
+                if password == st.secrets.get("app", {}).get("password", ""):
+                    st.session_state.authenticated = True
+                    st.query_params["auth"] = "1"
+                    st.rerun()
+                else:
+                    st.error("Wrong password")
+        st.stop()
 
 
 NEWS_CATEGORIES = {
